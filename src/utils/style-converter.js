ed6042937cced9f441203e46f252eb60a136018c
TakeshiSugimori <bigblog090@gmail.com>
2025-04-25 13:42:28 +0900

    fix comment

--- src/utils/style-converter.js
+++ src/utils/style-converter.js
@@ -2,8 +2,7 @@ export const convertStylesToCSS = (styles) => {
   const inlineStyles = {};
   const classes = [];
 
-  // margin と padding の処理を共通化する関数
-  // 2段ある場合
+  // obj_2
   const processSpacing = (property, spacingValues, prefixes) => {
     if (typeof spacingValues === 'object' && spacingValues !== null) {
       Object.entries(prefixes).forEach(([side, preval]) => {
@@ -13,20 +12,19 @@ export const convertStylesToCSS = (styles) => {
             if (value && value !== '---') {
               const prefix = prefixes[side];
               if (breakpoint === 'sm') {
-                classes.push(`${prefix}-${value}`); // smの場合はブレイクポイントなし
+                classes.push(`${prefix}-${value}`);
               } else {
-                classes.push(`${prefix}-${breakpoint}-${value}`); // md, lgなどはブレイクポイント付き
+                classes.push(`${prefix}-${breakpoint}-${value}`);
               }
             }
           });
         }
       });
     } else if (spacingValues) {
-      // 単一値の場合はインラインスタイルに設定（後方互換性）
       inlineStyles[property] = spacingValues;
     }
   };
-  // 1段だけの場合
+  // obj_1
   const generateBreakpointClasses = (prefix, value) => {
     if (typeof value === 'object' && value !== null) {
       Object.entries(value).forEach(([breakpoint, val]) => {
@@ -42,25 +40,14 @@ export const convertStylesToCSS = (styles) => {
       classes.push(`${prefix}${value}`);
     }
   };
-  // 0段の場合
+  // obj_0
   const generateClasses = (prefix, value) => {
     if (typeof value !== null && value !== '---') {
       classes.push(`${prefix}-${value}`);
     }
   };
-  // positionの場合
+  // position
   const getPositionClass = (alignment, isOutsideHorizontal, isOutsideVertical) => {
-    // const classMap = {
-    //   'top left': isHalfOutside ? 'oui_top-0 oui_start-0 oui_translate-middle' : 'oui_top-0 oui_start-0',
-    //   'top center': isHalfOutside ? 'oui_top-0 oui_start-50 oui_translate-middle' : 'oui_top-0 oui_start-50 oui_translate-middle-x',
-    //   'top right': isHalfOutside ? 'oui_top-0 oui_start-100 oui_translate-middle' : 'oui_top-0 oui_end-0',
-    //   'center left': isHalfOutside ? 'oui_top-50 oui_start-0 oui_translate-middle' : 'oui_top-50 oui_start-0 oui_translate-middle-y',
-    //   'center center': isHalfOutside ? 'oui_top-50 oui_start-50 oui_translate-middle' : 'oui_top-50 oui_start-50 oui_translate-middle',
-    //   'center right': isHalfOutside ? 'oui_top-50 oui_start-100 oui_translate-middle' : 'oui_top-50 oui_end-0 oui_translate-middle-y',
-    //   'bottom left': isHalfOutside ? 'oui_top-100 oui_start-0 oui_translate-middle' : 'oui_bottom-0 oui_start-0',
-    //   'bottom center': isHalfOutside ? 'oui_top-100 oui_start-50 oui_translate-middle' : 'oui_bottom-0 oui_start-50 oui_translate-middle-x',
-    //   'bottom right': isHalfOutside ? 'oui_top-100 oui_start-100 oui_translate-middle' : 'oui_bottom-0 oui_end-0',
-    // };
     if (alignment === 'top left') {
       if (isOutsideHorizontal && !isOutsideVertical) {
         classes.push('oui_top-0 oui_start-0 oui_translate-middle-x');
@@ -160,12 +147,10 @@ export const convertStylesToCSS = (styles) => {
   if (styles?.base?.offcanvasBgColor) {
     inlineStyles['--oui-offcanvas-bgcolor'] = styles.base.offcanvasBgColor;
   }
-
-  // styles.base.sizing が存在するか確認
+  
   if (styles?.base?.sizing) {
     const { size, width, height, column, offset, parentBlockName } = styles.base.sizing;
 
-    // そのまま出力するものは直接入れる
     if (size) {
       inlineStyles['--oui-size'] = size;
     }
@@ -223,27 +208,19 @@ export const convertStylesToCSS = (styles) => {
   // styles.base.spacing が存在するか確認
   if (styles?.base?.spacing) {
     const { space, margin, padding, gap } = styles.base.spacing;
-    // margin の接頭辞マップ
     const marginPrefixes = { top: 'oui_mt', bottom: 'oui_mb', left: 'oui_ml', right: 'oui_mr' };
-    // padding の接頭辞マップ
     const paddingPrefixes = { top: 'oui_pt', bottom: 'oui_pb', left: 'oui_pl', right: 'oui_pr' };
-    // gapの接頭辞
     const gapPrefixes = { row: 'oui_row-gap', column: 'oui_column-gap' };
 
-    // space
     if (space) {
       inlineStyles['--oui-space'] = space;
     }
-
-    // margin の処理
     if (margin) {
       processSpacing('oui_margin', margin, marginPrefixes);
     }
-    // padding の処理
     if (padding) {
       processSpacing('oui_padding', padding, paddingPrefixes);
     }
-    // gapの処理
     if (gap) {
       processSpacing('oui_gap', gap, gapPrefixes);
     }
@@ -260,11 +237,9 @@ export const convertStylesToCSS = (styles) => {
     }
   }
 
-  // styles.base.sizing が存在するか確認
   if (styles?.base?.display) {
     const { header, container, row, grid, overflow, opacity, zIndex, visible } = styles.base.display;
 
-    // そのまま出力するものは直接入れる
     if (header) {
       generateClasses('oui_header', header);
     }
