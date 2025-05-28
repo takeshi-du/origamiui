a3ac67f8c7e9966f663a5d495c546127080e2536
TakeshiSugimori <bigblog090@gmail.com>
2025-05-28 09:48:17 +0900

    fix custom-container

--- build/blocks/custom-container/index.asset.php
+++ build/blocks/custom-container/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '5ad40d0098df53fde5c4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'a73c7722a5ca84b259d5');
