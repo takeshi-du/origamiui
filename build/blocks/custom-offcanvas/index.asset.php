d6ddf238c64b34f6a8e74ba71cd948e9b5e854c7
TakeshiSugimori <bigblog090@gmail.com>
2025-05-28 10:32:18 +0900

    fix custom-offcanvas

--- build/blocks/custom-offcanvas/index.asset.php
+++ build/blocks/custom-offcanvas/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f59249c2d42ed6292234');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'd87caf09b4c0b4536406');
--- src/blocks/custom-offcanvas/edit.js
+++ src/blocks/custom-offcanvas/edit.js
@@ -1,23 +1,23 @@
 import { useMemo } from 'react';
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, useInnerBlocksProps, InspectorControls, ButtonBlockAppender } from '@wordpress/block-editor';
-import { PanelBody, SelectControl, TabPanel, __experimentalUnitControl as UnitControl, __experimentalHeading as Heading, Flex, FlexItem, ColorPicker } from '@wordpress/components';
+import { PanelBody, SelectControl, TabPanel, __experimentalUnitControl as UnitControl, __experimentalHeading as Heading, ColorPicker } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { convertStylesToCSS } from '../../utils/style-converter';
 import cloneDeep from 'lodash/cloneDeep';
 import set from 'lodash/set';
+
+// 追加: 共通コンポーネント
+import LayoutFlexSettingsPanel from '../../components/LayoutFlexSettingsPanel';
 
 export default function Edit({ attributes, setAttributes, clientId }){
   const {
     offcanvasType,
     offcanvasPosition,
     offcanvasBreakPoint,
-    offcanvasBgColor,
     styles
   } = attributes;
   const breakpoints = ['sm', 'md', 'lg'];
-  const marginSides = ['top', 'bottom', 'left', 'right']; // marginの方向
-  const gapSides = ['row', 'column']; // gapの方向
 
   // スタイルを変換（useMemoで最適化）
   const { inlineStyles, blockClasses } = useMemo(() => {
@@ -60,7 +60,7 @@ export default function Edit({ attributes, setAttributes, clientId }){
   return (
     <>
       <InspectorControls>
-        <PanelBody title={__('Offcanvas Settings', 'origamiui')}>
+        <PanelBody title={__('Offcanvas Settings', 'origamiui')} initialOpen={true}>
           <SelectControl
             label={__('Offcanvas Type', 'origamiui')}
             value={offcanvasType}
@@ -122,146 +122,11 @@ export default function Edit({ attributes, setAttributes, clientId }){
             )}
           </TabPanel>
         </PanelBody>
-        <PanelBody title={__('Flex&Grid Settings', 'origamiui')} initialOpen={false}>
-          <TabPanel
-            tabs={tabs}
-            onSelect={() => {}} // タブ選択時の処理は不要
-          >
-            {(tab) => (
-              <>
-                <Heading style={{ marginTop: '1.5em' }}>{__(`Flex Display Settings for ${tab.title}`, 'origamiui')}</Heading>
-                <SelectControl
-                  label={__(`Flex Display (${tab.title})`, 'origamiui')}
-                  value={styles.base.flex.display[tab.name]}
-                  options={[
-                    { label: '---', value: '---' },
-                    { label: 'flex', value: 'flex' },
-                    { label: 'inline-flex', value: 'inline-flex' }
-                  ]}
-                  onChange={(newDisplay) => updateStyles(`base.flex.display.${tab.name}`, newDisplay)}
-                />
-                <Flex style={{flexWrap: 'wrap'}}>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Flex Direction (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.direction[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'row', value: 'row' },
-                        { label: 'column', value: 'column' }
-                      ]}
-                      onChange={(newDirection) => updateStyles(`base.flex.direction.${tab.name}`, newDirection)}
-                    />
-                  </FlexItem>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Flex Wrap (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.wrap[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'no wrap', value: 'nowrap' },
-                        { label: 'wrap', value: 'wrap' }
-                      ]}
-                      onChange={(newWrap) => updateStyles(`base.flex.wrap.${tab.name}`, newWrap)}
-                    />
-                  </FlexItem>
-                </Flex>
-                <Flex style={{flexWrap: 'wrap'}}>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Align Items (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.align[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'start', value: 'start' },
-                        { label: 'end', value: 'end' },
-                        { label: 'center', value: 'center' },
-                        { label: 'baseline', value: 'baseline' },
-                        { label: 'stretch', value: 'stretch' }
-                      ]}
-                      onChange={(newAlign) => updateStyles(`base.flex.align.${tab.name}`, newAlign)}
-                    />
-                  </FlexItem>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Justify Content (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.justify[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'start', value: 'start' },
-                        { label: 'end', value: 'end' },
-                        { label: 'center', value: 'center' },
-                        { label: 'between', value: 'between' },
-                        { label: 'around', value: 'around' },
-                        { label: 'evenry', value: 'evenry' }
-                      ]}
-                      onChange={(newJustify) => updateStyles(`base.flex.justify.${tab.name}`, newJustify)}
-                    />
-                  </FlexItem>
-                </Flex>
-                <Flex style={{flexWrap: 'wrap'}}>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Flex Grow (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.grow[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'grow', value: 'grow-1' },
-                        { label: 'no grow', value: 'grow-0' }
-                      ]}
-                      onChange={(newGrow) => updateStyles(`base.flex.grow.${tab.name}`, newGrow)}
-                    />
-                  </FlexItem>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Flex Shrink (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.shrink[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'shrink', value: 'shrink-1' },
-                        { label: 'no shrink', value: 'shrink-0' }
-                      ]}
-                      onChange={(newShrink) => updateStyles(`base.flex.shrink.${tab.name}`, newShrink)}
-                    />
-                  </FlexItem>
-                </Flex>
-                <Flex style={{flexWrap: 'wrap'}}>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Flex Self (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.self[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: 'start', value: 'start' },
-                        { label: 'end', value: 'end' },
-                        { label: 'center', value: 'center' },
-                        { label: 'baseline', value: 'baseline' },
-                        { label: 'stretch', value: 'stretch' }
-                      ]}
-                      onChange={(newSelf) => updateStyles(`base.flex.self.${tab.name}`, newSelf)}
-                    />
-                  </FlexItem>
-                  <FlexItem style={{width: '45%'}}>
-                    <SelectControl
-                      label={__(`Flex Order (${tab.title})`, 'origamiui')}
-                      value={styles.base.flex.order[tab.name]}
-                      options={[
-                        { label: '---', value: '---' },
-                        { label: '0', value: '0' },
-                        { label: '1', value: '1' },
-                        { label: '2', value: '2' },
-                        { label: '3', value: '3' },
-                        { label: '4', value: '4' },
-                        { label: '5', value: '5' }
-                      ]}
-                      onChange={(newOrder) => updateStyles(`base.flex.order.${tab.name}`, newOrder)}
-                    />
-                  </FlexItem>
-                </Flex>
-              </>
-            )}
-          </TabPanel>
-        </PanelBody>
+        <LayoutFlexSettingsPanel
+          styles={ styles.base.flex }
+          updateStyles={ updateStyles }
+          initialOpen={ false }
+        />
       </InspectorControls>
       
       <div {...innerBlocksProps} />
